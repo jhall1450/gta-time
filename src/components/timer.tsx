6a8659a14d71c6285d4ae40ccee3cd1d4020296c
Justin Hall <jhall1450@outlook.com>
2022-04-09 16:12:56 -0400

    initial commit

---
+++ src/components/timer.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Timer = () => {
+  const [timeSince, setTimeSince] = useState(0);
+
+  setInterval(() => setTimeSince(
+    (Date.now() / 1000) - 1383235200
+  ));
+
+  const secondsInGame = timeSince * 30
+  const yearsInGame = secondsInGame / 3.154e+7
+  const monthsInGame = (yearsInGame - Math.floor(yearsInGame)) * 12
+  const daysInGame = ((monthsInGame - Math.floor(monthsInGame)) * 30.4167)
+  const hoursInGame = ((daysInGame - Math.floor(daysInGame)) * 24)
+  const minutesInGame = ((hoursInGame - Math.floor(hoursInGame)) * 60)
+
+  return (
+    <>
+    <p>Years in game: {Math.floor(yearsInGame)}</p>
+    <p>Months in game: {Math.floor(monthsInGame)}</p>
+    <p>Days in game: {Math.floor(daysInGame)}</p>
+    <p>Hours in game: {Math.floor(hoursInGame)}</p>
+    <p>Minutes in game: {Math.floor(minutesInGame)}</p>
+    </>
+  )
+}
+
+export default Timer
